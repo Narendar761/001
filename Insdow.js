0a2a870b20512e35b11d73fedd9c809328ced96b
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-21 18:49:43 +0530

    Create Insdow.js

---
+++ Insdow.js
@@ -0,0 +1,20 @@
+{
+  "data": {
+    "caption": " title",
+    "media": [
+      {
+        "thumbnail": "thumbnail link",
+        "type": "video",
+        "url": "download link"
+      }
+    ],
+    "metadata": {
+      "shortcode": "DJgcMsLICGP",
+      "url": "https://www.instagram.com/reel/DJgcMsLICGP/?utm_source=ig_web_copy_link"
+    },
+    "type": "video",
+    "username": "geeturoyal_"
+  },
+  "owner": "https://t.me/bdbots",
+  "status": "success"
+}
