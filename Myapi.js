afbca8e6117758971d30713468882be552b4fc65
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-26 09:24:30 +0530

    Create Myapi.js

---
+++ Myapi.js
@@ -0,0 +1,17 @@
+{
+  "Extracted Info": [
+    {
+      "Direct Download Link": "download link",
+      "Size": "1926.93 MB",
+      "Thumbnails": {
+        "140x90": " thumbnail link 140×90",
+        "360x270": "thumbnail link 360×270",
+        "60x60": "thumbnail link 60×60",
+        "850x580": "thumbnail link 850×580"
+      },
+      "Title": "title.mkv"
+    }
+  ],
+  "ShortLink": "https://1024terabox.com/s/1mmKUINfI54JxIhu-FAeOZA",
+  "status": "success"
+}
