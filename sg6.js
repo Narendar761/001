dbe4a9c3e55723922e5021e1c73348c403f40627
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-27 16:20:41 +0530

    Create sg6.js

---
+++ sg6.js
@@ -0,0 +1,29 @@
+{
+  "status": "success",
+  "data": {
+    "structure": {
+      "file_name": "RadhaKrishna Episode 1020 on JioHotstar.mp4",
+      "size": "39.06 MB",
+      "thumb": "https://data.terabox.app/thumbnail/79ec07524a1d8bc2785cbcfb55a31418?fid=4400021660661-250528-801143081947396&time=1748340000&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-jVxay0E5%2F121hE5t1NqGpEyzwzI%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=9168906485640892997&dp-callid=0&size=c850_u580&quality=100&vuk=-&ft=video",
+      "dlink": null,
+      "fs_id": "801143081947396",
+      "download_url": "https://d.terabox.app/file/79ec07524a1d8bc2785cbcfb55a31418?fid=4400021660661-250528-801143081947396&dstime=1748342802&rt=sh&sign=FDtAER-DCb740ccc5511e5e8fedcff06b081203-D4hArDynGuiJfZRd%2ByuuatMKIds%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=9168906485640892997&dp-callid=0&r=720036588&sh=1&region=jp",
+      "direct_link": "https://d8.freeterabox.com/file/79ec07524a1d8bc2785cbcfb55a31418?bkt=en-2fb6763f1c8fb10120ea2029edeb5b7d709a9098d0a491571bf092d4fd2b9fc506926b37db8af4b4&xcode=1d7a789799c47843b7454ecd8ec4e9a0f1ea0126dd18cee89002980e0de3ece6a7c602c8f11ea26740edd2a527bc1733&fid=4400021660661-250528-801143081947396&time=1748342802&sign=FDTAXUGERQlBHSKfon-DCb740ccc5511e5e8fedcff06b081203-gW%2Fc68BsLUnbpdklONu7Yut8td4%3D&to=d8&size=40960068&sta_dx=40960068&sta_cs=7&sta_ft=mp4&sta_ct=0&sta_mt=0&fm2=MH%2Ctky%2CAnywhere%2C%2CTWFoYXJhc2h0cmE%3D%2Cany&region=tky&ctime=1748305003&mtime=1748339134&resv0=-1&resv1=0&resv2=rlim&resv3=5&resv4=40960068&vuk=4401062552107&iv=2&htype=&randtype=&newver=1&newfm=1&secfm=1&flow_ver=3&pkey=en-7914b64c247f48ab280dccc17442aa7af78056181beb2258428c155e02cc9d66902e3ae36503ea40&expires=1748371602&rt=sh&r=720036588&sh=1&vbdid=-&fin=RadhaKrishna_Episode_1020_on_JioHotstar.mp4&fn=RadhaKrishna_Episode_1020_on_JioHotstar.mp4&rtype=1&dp-logid=9168906485640892997&dp-callid=0.1&hps=1&tsl=0&csl=0&fsl=-1&csign=WhHr45uDlWAKE%2BCgBUWUY4FmKFE%3D&so=0&ut=1&uter=4&serv=1&uc=173034443&ti=14a3010384c1ca3cd0a29c27c94823961ea88d4a48b24d15305a5e1275657320&tuse=&raw_appid=0&ogr=0&rregion=XVVi&adg=&reqlabel=250528_f_1f5406149dcc5e2c584155313f53903b_-1_5c5ff5330dfaeb771114bc9db6814896&ccn=IN&by=themis",
+      "stream_url": "https://www.terabox.app/share/extstreaming.m3u8?uk=4400021660661&shareid=45220989082&type=M3U8_AUTO_360&fid=801143081947396&sign=DCb740ccc5511e5e8fedcff06b081203&timestamp=1748342802&jsToken=6D0CC0BD85A977227CF3B8D06BCA1FB3799ABEA7EF1EE8768E292D3BA18DFDB12F9B449C5748487E8BD829A72235FCF86D6ED4A8AFB83642E4E2F7BB4940524F&esl=1&isplayer=1&ehps=1&clienttype=0&app_id=250528&web=1&channel=dubox",
+      "sizebytes": 40960068
+    },
+    "all_files": [
+      {
+        "file_name": "RadhaKrishna Episode 1020 on JioHotstar.mp4",
+        "size": "39.06 MB",
+        "thumb": "https://data.terabox.app/thumbnail/79ec07524a1d8bc2785cbcfb55a31418?fid=4400021660661-250528-801143081947396&time=1748340000&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-jVxay0E5%2F121hE5t1NqGpEyzwzI%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=9168906485640892997&dp-callid=0&size=c850_u580&quality=100&vuk=-&ft=video",
+        "dlink": null,
+        "fs_id": "801143081947396",
+        "download_url": "https://terabox-url-fixer.mohdamir7505.workers.dev/?url=https://d.terabox.app/file/79ec07524a1d8bc2785cbcfb55a31418?fid=4400021660661-250528-801143081947396&dstime=1748342802&rt=sh&sign=FDtAER-DCb740ccc5511e5e8fedcff06b081203-D4hArDynGuiJfZRd%2ByuuatMKIds%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=9168906485640892997&dp-callid=0&r=720036588&sh=1&region=jp",
+        "direct_link": "https://terabox-url-fixer.mohdamir7505.workers.dev/?url=https://d8.freeterabox.com/file/79ec07524a1d8bc2785cbcfb55a31418?bkt=en-2fb6763f1c8fb10120ea2029edeb5b7d709a9098d0a491571bf092d4fd2b9fc506926b37db8af4b4&xcode=1d7a789799c47843b7454ecd8ec4e9a0f1ea0126dd18cee89002980e0de3ece6a7c602c8f11ea26740edd2a527bc1733&fid=4400021660661-250528-801143081947396&time=1748342802&sign=FDTAXUGERQlBHSKfon-DCb740ccc5511e5e8fedcff06b081203-gW%2Fc68BsLUnbpdklONu7Yut8td4%3D&to=d8&size=40960068&sta_dx=40960068&sta_cs=7&sta_ft=mp4&sta_ct=0&sta_mt=0&fm2=MH%2Ctky%2CAnywhere%2C%2CTWFoYXJhc2h0cmE%3D%2Cany&region=tky&ctime=1748305003&mtime=1748339134&resv0=-1&resv1=0&resv2=rlim&resv3=5&resv4=40960068&vuk=4401062552107&iv=2&htype=&randtype=&newver=1&newfm=1&secfm=1&flow_ver=3&pkey=en-7914b64c247f48ab280dccc17442aa7af78056181beb2258428c155e02cc9d66902e3ae36503ea40&expires=1748371602&rt=sh&r=720036588&sh=1&vbdid=-&fin=RadhaKrishna_Episode_1020_on_JioHotstar.mp4&fn=RadhaKrishna_Episode_1020_on_JioHotstar.mp4&rtype=1&dp-logid=9168906485640892997&dp-callid=0.1&hps=1&tsl=0&csl=0&fsl=-1&csign=WhHr45uDlWAKE%2BCgBUWUY4FmKFE%3D&so=0&ut=1&uter=4&serv=1&uc=173034443&ti=14a3010384c1ca3cd0a29c27c94823961ea88d4a48b24d15305a5e1275657320&tuse=&raw_appid=0&ogr=0&rregion=XVVi&adg=&reqlabel=250528_f_1f5406149dcc5e2c584155313f53903b_-1_5c5ff5330dfaeb771114bc9db6814896&ccn=IN&by=themis",
+        "stream_url": "https://www.terabox.app/share/extstreaming.m3u8?uk=4400021660661&shareid=45220989082&type=M3U8_AUTO_360&fid=801143081947396&sign=DCb740ccc5511e5e8fedcff06b081203&timestamp=1748342802&jsToken=6D0CC0BD85A977227CF3B8D06BCA1FB3799ABEA7EF1EE8768E292D3BA18DFDB12F9B449C5748487E8BD829A72235FCF86D6ED4A8AFB83642E4E2F7BB4940524F&esl=1&isplayer=1&ehps=1&clienttype=0&app_id=250528&web=1&channel=dubox",
+        "sizebytes": 40960068
+      }
+    ]
+  }
+}
